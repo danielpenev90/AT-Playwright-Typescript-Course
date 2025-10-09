5d18700cab666decbce10f533879f8d4294c3727
DanielPenev <danielpenev90@gmail.com>
2025-10-09 22:26:08 +0300

    create tsconfig

---
+++ index.ts
@@ -0,0 +1,20 @@
+console.log("Hello! My first ts code.")
+
+const age: number = 25;
+
+let isTrue: boolean = 5 > 7;
+console.log(isTrue);
+
+let singer: null = null;
+
+let country;
+country = 15;
+
+console.log(country);
+
+    let lName: any = 5>8;
+
+    let numbers: number []= [10, 20];
+    let combined: (string | number)[] = [10,50, "Hello"];
+
+
