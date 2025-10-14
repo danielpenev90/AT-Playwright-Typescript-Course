827911c447c1891cc9e1e5fcd4638f48b8ead318
DanielPenev <danielpenev90@gmail.com>
2025-10-14 22:11:30 +0300

    Added arrays examples

---
+++ arrays.ts
@@ -0,0 +1,26 @@
+// arrays declaration
+let numsArray: number[] = [1, 2, 3, 4, 5];
+let stringsArray: string[] = ['Pesho', 'Gosho', 'Tosho', 'Ivan'  ];
+let combinedArray: (string | number)[] = [1, 'Pesho', 'Ivan', 2];
+
+//accessing array elements
+stringsArray[0]; //Pesho
+stringsArray[1]; //Gosho
+combinedArray[2]; //Ivan
+
+//acces array length
+numsArray.length;
+
+//method map    
+let multiplyNums = numsArray.map((num: number) => num * 2);
+let mapNames = stringsArray.map((currName: string) => `Hello ${currName}`);
+let checkTypeOf = combinedArray.map((element: number | string) => {
+    if (typeof element === 'number') {
+        return element + 5;}
+    else {
+        return `Welcome ${element}`;}
+});
+
+//console.log(multiplyNums);
+//console.log(mapNames);
+console.log(checkTypeOf);
