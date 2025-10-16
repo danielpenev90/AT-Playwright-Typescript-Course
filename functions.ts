6053f185186ee0cd92e7b99905dbab7f52cd9f8a
DanielPenev <danielpenev90@gmail.com>
2025-10-16 21:54:35 +0300

    Funktions

---
+++ functions.ts
@@ -0,0 +1,21 @@
+//Functions
+function displayPersonalData(name: string, age: number) {
+    return `${name} is ${age} years old.`;
+}
+
+    displayPersonalData("Daniel", 20);
+    console.log(displayPersonalData("Daniel", 20));
+
+    const personalData = displayPersonalData("Peter", 25);
+    console.log(personalData);
+
+function greetingVoid(): void {
+    console.log(`This function doesn't return anything!(type void)`);
+}
+
+function devideNums(numOne: number, numTwo: number): number {
+    return numOne / numTwo;
+}
+devideNums(10, 5);
+devideNums(50, 5);
+devideNums(100, 5);
